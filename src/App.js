f026dd020df2bb2d61c3a1e93aa583159f5cf128
mustafa <mustafa.sadriwala@utdallas.edu>
2021-02-04 21:31:13 -0600

    add react-bootstrap and jumbotron header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './styles/App.css';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <Jumbotron fluid>
+      <Container>
+        <h1>Oscar Wilde</h1>
+        <p>
+          This website was created by Dr. Kenneth Brewer's Spring 2021 Oscar Wilde class
+        </p>
+      </Container>
+    </Jumbotron>
   );
 }
 
