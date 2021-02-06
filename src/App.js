5b4d87a79a9a669032ca2427077a01f8664c0fb6
mustafa <mustafa.sadriwala@utdallas.edu>
2021-02-06 09:38:20 -0600

    add borders and background-color

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './styles/App.css';
-import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
-    <Jumbotron fluid>
-      <Container>
-        <h1>Oscar Wilde</h1>
-        <p>
-          This website was created by Dr. Kenneth Brewer's Spring 2021 Oscar Wilde class
-        </p>
-      </Container>
-    </Jumbotron>
+    <div className="outer-border">
+      <div className="inner-border">
+        <Container>
+          <h1>Oscar Wilde</h1>
+          <p>
+            This website was created by Dr. Kenneth Brewer's Spring 2021 Oscar Wilde class
+          </p>
+        </Container>
+      </div>
+    </div>
   );
 }
 
