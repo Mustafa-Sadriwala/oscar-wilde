ff9f56f9a5be59023a523ea981c085690fef1f07
mustafa <mustafa.sadriwala@utdallas.edu>
2021-02-13 01:07:30 -0600

    lorem ipsum

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,17 +16,17 @@ export default function Home() {
                             <div style={{ textAlign: "center" }}>
                                 <h2>Table of Contents</h2>
                             </div>
-                            <div style={{paddingLeft: 20, paddingTop: 20}}>
+                            <div style={{ paddingLeft: 20, paddingTop: 20 }}>
                                 <Link to="/wilde-websites" className="highlight">
                                     <h4>Chapter One: Wilde Websites</h4>
                                 </Link>
                             </div>
-                            <div style={{paddingLeft: 20, paddingTop: 20}}>
+                            <div style={{ paddingLeft: 20, paddingTop: 20 }}>
                                 <Link to="/wilde-era-websites" className="highlight">
                                     <h4>Chapter Two: Websites on Wilde's Era</h4>
                                 </Link>
                             </div>
-                            <div style={{paddingLeft: 20, paddingTop: 20}}>
+                            <div style={{ paddingLeft: 20, paddingTop: 20 }}>
                                 <Link to="/adaptations" className="highlight">
                                     <h4>Chapter Three: Adaptations of Wilde</h4>
                                 </Link>
@@ -35,6 +35,13 @@ export default function Home() {
                         <Col>
                             <div style={{ textAlign: "center" }}>
                                 <h2>Acknowledgements</h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat urna. Morbi a bibendum metus.
+                                    Donec scelerisque sollicitudin enim eu venenatis. Duis tincidunt laoreet ex, in pretium orci vestibulum eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis pharetra luctus lacus ut vestibulum. Maecenas ipsum lacus, lacinia quis posuere ut, pulvinar vitae dolor.
+                                    Integer eu nibh at nisi ullamcorper sagittis id vel leo. Integer feugiat faucibus libero, at maximus nisl suscipit posuere. Morbi nec enim nunc.
+                                    Phasellus bibendum turpis ut ipsum egestas, sed sollicitudin elit convallis.
+                                    Cras pharetra mi tristique sapien vestibulum lobortis. Nam eget bibendum metus, non dictum mauris. Nulla at tellus sagittis, viverra est a, bibendum metus.
+                                </p>
                             </div>
                         </Col>
                     </Row>
